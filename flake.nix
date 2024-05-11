81409ddd2f1e16682f844e37f0c8a3cf8b0da906
Dennis Bogers <dennis@bogers.xyz>
2024-05-11 15:10:54 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A flake for my personal desktop machine";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      boneweevil = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+           home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.denzo = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "denzo";
+  home.homeDirectory = "/home/denzo";
+
+  home.packages = with pkgs; [
+    _1password-gui
+  ];
+
+  imports = [
+    ./zsh.nix
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+    # Install firefox.
+  programs.firefox = {
+    enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+  };
+
+  # Configure my IDE, VSCode
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dbaeumer.vscode-eslint
+      eamodio.gitlens
+      gitlab.gitlab-workflow
+      hashicorp.terraform
+      jnoortheen.nix-ide
+      mkhl.direnv
+      ms-azuretools.vscode-docker
+      octref.vetur
+      redhat.vscode-yaml
+      rust-lang.rust-analyzer
+      yzhang.markdown-all-in-one
+      zhuangtongfa.material-theme
+    ];
+  };
+}
